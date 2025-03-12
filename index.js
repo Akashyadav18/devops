68dd71758cb1044c616bdcb708e13f8b868d46b9
Akashyadav18 <131423993+Akashyadav18@users.noreply.github.com>
2025-03-12 15:03:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("Hello from JS")
-console.log("Hello from Akash")
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, icon: '', keepAll: false, reportDir: '', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: '', useWrapperFileDirectly: true])
+            }
+        }
+    }
+}
